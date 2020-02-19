6b2241033221b7beab67e00066eab9a5ed2f2cd3
Daniel Muresan <daniel.muresan@avaelgo.ro>
2020-02-19 13:47:53 +0200

    researching for tools to implement my JSON API

--- Papiku/Core/DBServices/JSONServices/JSONService.cs
+++ Papiku/Core/DBServices/JSONServices/JSONService.cs
@@ -6,6 +6,12 @@ using System.IO;
 using static System.Console;
 using Papiku.BusinessLogic;
 using Newtonsoft.Json.Converters;
+using System.Linq;
+using System.Collections;
+using System.Resources;
+using System.Reflection;
+using Papiku.Properties;
+using static Papiku.Properties.Resources;
 
 namespace Papiku.Core.DBServices.JSONServices
 {
@@ -13,51 +19,29 @@ namespace Papiku.Core.DBServices.JSONServices
     {
         public static void test()
         {
-            JObject o = JObject.Parse(@"{
-  'Stores': [
-    'Lambton Quay',
-    'Willis Street'
-  ],
-  'Manufacturers': [
-    {
-      'Name': 'Acme Co',
-      'Products': [
-        {
-          'Name': 'Anvil',
-          'Price': 50
-        }
-      ]
-    },
-    {
-      'Name': 'Contoso',
-      'Products': [
-        {
-          'Name': 'Elbow Grease',
-          'Price': 99.95
-        },
-        {
-          'Name': 'Headlight Fluid',
-          'Price': 4
-        }
-      ]
-    }
-  ]
-}");
+            /***
+             * serialize => object to JSON format string
+             * deserialize => JSON format string to object
+             * [OnSerializing], [OnSerialized] => annotations for serialize callbacks
+             * JObject.FromObject( .NET Type object) => converts from any object straight into JSON object
+             * LINQ => SQL keywords for iteration through the JSON properties and more
+             * parse => still a converter,but i'm not allowed to use it
+             * 
+             * using Papiku.Properties;
+             * using static Papiku.Properties.Resources;
+             * ResourceManager rm = new ResourceManager(typeof(Resx_File_Name)); => String resources just like in Kotlin
+             */
 
-            string name = (string)o.SelectToken("Manufacturers[0].Name");
+            ResourceManager rm = new ResourceManager(typeof(Resources));
 
-            Console.WriteLine(name);
-            // Acme Co
 
-            decimal productPrice = (decimal)o.SelectToken("Manufacturers[0].Products[0].Price");
+            string key = rm.GetString(String1);
+            Menu m = new Menu();
+            m.description = "asdasd";
+            JObject o = JObject.FromObject(m);
 
-            Console.WriteLine(productPrice);
-            // 50
-
-            string productName = (string)o.SelectToken("Manufacturers[1].Products[0].Name");
-
-            Console.WriteLine(productName);
-            // Elbow Grease
+             WriteLine(o[DESCRIPTION]);
+            
         }
     }
 }
